6d92b2507cbfdcee7a71219c0e0142e51a3266d0
Rodrigo Rodriguez <rodrigo.edwin@hotmail.com>
2023-01-30 10:33:39 -0700

    api completed

---
+++ swagger.js
@@ -0,0 +1,22 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'My API',
+    description: 'Contacts API'
+  },
+  host: 'cse-341-medium-example-w23.onrender.com',
+  // host: 'localhost:8080',
+  schemes: ['https', 'http']
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+
+// generate swagger.json
+swaggerAutogen(outputFile, endpointsFiles, doc);
+
+// Run server after it gets generated
+// swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
+//   await import('./index.js');
+// });
